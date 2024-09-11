7e391dfd8ca765a1a41e881e6d4a99343241fea8
viktorZag <viktorzahurskyi@gmail.com>
2024-09-11 09:38:50 +0300

    fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,25 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 const Home = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.commento.io/js/commento.js";
-    script.async = true;
-    script.setAttribute("data-no-fonts", "true"); // Optional attribute
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return <div id="commento" />;
-  // return (
-  //   <div>
-  //     <h1>Home</h1>
-  //   </div>
-  // );
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default Home;
