383e40f5d59e6fe2b5dd5138c955d1c973fb1eb3
viktorZag <viktorzahurskyi@gmail.com>
2024-09-11 07:36:07 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product/:id" element={<Product />} />
       </Routes>
     </Router>
   );
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Commento from "../components/Commento";
 
-const useQuery = () => {
-  return new URLSearchParams(useLocation().search);
-};
-
 const Product = () => {
-  //   const { id } = useParams();
-  let query = useQuery();
-  const id = query.get("id");
-  return <Commento pageId={id} />;
+  const { id } = useParams();
+  return <h1>{id}</h1>;
+  //   return <Commento pageId={id} />;
 };
 
 export default Product;
