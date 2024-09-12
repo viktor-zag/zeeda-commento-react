2b8d05da1a8b6c8c9b6c0e4b05394f24a2877414
viktorZag <viktorzahurskyi@gmail.com>
2024-09-12 05:33:07 +0300

    update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
 
 const Home = () => {
+  let query = useQuery();
+  const productId = query.get("id");
+
   return (
     <div>
-      <iframe src="https://zeeda-commento.vercel.app/product/2"></iframe>
+      <iframe
+        src={"https://zeeda-commento.vercel.app/product/" + productId}
+        width="100%"
+        height="100%"
+      ></iframe>
     </div>
   );
 };
